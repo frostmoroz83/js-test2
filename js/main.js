38467bf88ddcede23d238954ddf1f01c00e5ed22
Alexander Morozov <frostmoroz83@yandex.ru>
2019-03-22 14:37:21 +0400

    first commit2

---
+++ js/main.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  'use strict';
+
+  let tab = document.querySelectorAll('.info-header-tab'),
+    info = document.querySelector('.info-header'),
+    tabContent = document.querySelectorAll('.info-tabcontent');
+
+  function hidetabContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove('show');
+      tabContent[i].classList.add('hide');
+    }
+  }
+
+  hidetabContent(1);
+
+    function showTabContent(b) {
+      if (tabContent[b].classList.contains('hide')) {
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+      }
+    }
+
+    info.addEventListener('click', function(e) {
+      let target = e.target;
+      if (target && target.classList.contains('info-header-tab')) {
+        for(let i = 0; i < tab.length; i++) {
+          if (target == tab[i]) {
+            hidetabContent(0);
+            showTabContent(i);
+            break;
+          }
+        }
+      }
+    });
+});
