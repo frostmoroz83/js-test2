d39f2447d21b9f561e5d66efdf533d5fae14c284
Alexander Morozov <frostmoroz83@yandex.ru>
2019-03-29 10:35:27 +0400

    доработка отправки формы

--- js/main.js
+++ js/main.js
@@ -117,86 +117,63 @@ window.addEventListener('DOMContentLoaded', function () {
   };
 
   let form = document.querySelector('.main-form'),
-    input = form.getElementsByTagName('input'),
-    statusMessage = document.createElement('div');
+  formEmail = document.getElementById('form'),
+    
+    statusMessage = document.createElement('div'),
+    inputEmail = formEmail.getElementsByTagName('input');
 
   statusMessage.classList.add('status');
 
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    form.appendChild(statusMessage);
+  sendForm(form);
+  sendForm(formEmail);
 
-    let request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-    // request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded'); php формат данных
-    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
-    let formData = new FormData(form);
-    let obj = {};
-    formData.forEach(function (value, key) {
-      obj[key] = value;
-    });
-    let json = JSON.stringify(obj);
-
-    // request.send(formData); php формат данных
-    request.send(json);
-
-    request.addEventListener('readystatechange', function () {
-      if (request.readyState < 4) {
-        statusMessage.innerHTML = message.loading;
-      } else if (request.readyState === 4 && request.status == 200) {
-        statusMessage.innerHTML = message.success;
-      } else {
-        statusMessage.innerHTML = message.failure;
+  function sendForm(elem) {
+    elem.addEventListener('submit', function (event) {
+      event.preventDefault();
+      elem.appendChild(statusMessage);
+  
+      let request = new XMLHttpRequest(),
+      input = elem.getElementsByTagName('input');
+      request.open('POST', 'server.php');
+      // request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded'); php формат данных
+      request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+  
+      let formData = new FormData(elem);
+      let obj = {};
+      formData.forEach(function (value, key) {
+        obj[key] = value;
+      });
+      let json = JSON.stringify(obj);
+  
+      // request.send(formData); php формат данных
+      request.send(json);
+  
+      request.addEventListener('readystatechange', function () {
+        if (request.readyState < 4) {
+          statusMessage.innerHTML = message.loading;
+        } else if (request.readyState === 4 && request.status == 200) {
+          statusMessage.innerHTML = message.success;
+        } else {
+          statusMessage.innerHTML = message.failure;
+        }
+      });
+  
+      for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
       }
+  
     });
 
-    for (let i = 0; i < input.length; i++) {
-      input[i].value = '';
-    }
+    
 
-  });
+
+  }
+
+  
 
   //contact form
-  let formEmail = document.getElementById('form'),
-  inputEmail = formEmail.getElementsByTagName('input');
+  
 
-  formEmail.addEventListener('submit', function (event) {
-    event.preventDefault();
-    formEmail.appendChild(statusMessage);
-    console.log(formEmail);
-    console.log(inputEmail);
-
-    let request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-    // request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded'); php формат данных
-    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
-    let formDataE = new FormData(formEmail);
-    let obj = {};
-    formDataE.forEach(function (value, key) {
-      obj[key] = value;
-    });
-    let jsonE = JSON.stringify(obj);
-
-    // request.send(formData); php формат данных
-    request.send(jsonE);
-
-    request.addEventListener('readystatechange', function () {
-      if (request.readyState < 4) {
-        statusMessage.innerHTML = message.loading;
-      } else if (request.readyState === 4 && request.status == 200) {
-        statusMessage.innerHTML = message.success;
-      } else {
-        statusMessage.innerHTML = message.failure;
-      }
-    });
-
-    for (let i = 0; i < inputEmail.length; i++) {
-      inputEmail[i].value = '';
-    }
-
-  });
 
 
   
